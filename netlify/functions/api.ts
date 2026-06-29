600feff6a3c9bace6ab49384f9d698bd0c762aee
Your Name <you@example.com>
2026-06-29 12:46:40 +0530

    Fix Express routing by adding basePath to serverless-http

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,4 +1,6 @@
 import serverless from 'serverless-http';
 import app from '../../src/app';
 
-export const handler = serverless(app);
+export const handler = serverless(app, {
+  basePath: '/.netlify/functions/api'
+});

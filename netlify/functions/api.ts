7965657fccb8eff071210cfaa2fc81670385914b
Your Name <you@example.com>
2026-06-29 15:03:10 +0530

    Remove Express 5 breaking wildcard route

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -4,14 +4,8 @@ import app from '../../src/app';
 
 const netlifyApp = express();
 
-netlifyApp.all('*', (req, res, next) => {
-  if (req.url === '/test-netlify') {
-    res.json({ message: 'Netlify function is working and intercepting requests!', url: req.url, originalUrl: req.originalUrl });
-  } else {
-    next();
-  }
-});
-
 netlifyApp.use(app);
 
-export const handler = serverless(netlifyApp);
+export const handler = serverless(netlifyApp, {
+  basePath: '/.netlify/functions'
+});

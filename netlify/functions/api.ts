e153c4a95e8869b3f18f2c8eba3ceac6bc55b854
Your Name <you@example.com>
2026-06-29 14:14:02 +0530

    Fix req.originalUrl for Express routing in Netlify

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -4,10 +4,20 @@ import app from '../../src/app';
 
 const netlifyApp = express();
 
-// Force add /api prefix if it's missing (Netlify strips the function name)
+netlifyApp.use((req, res, next) => {
+  if (req.url === '/debug') {
+    res.json({ url: req.url, originalUrl: req.originalUrl, path: req.path });
+  } else {
+    next();
+  }
+});
+
 netlifyApp.use((req, res, next) => {
   if (req.url && !req.url.startsWith('/api')) {
     req.url = '/api' + req.url;
+  }
+  if (req.originalUrl && !req.originalUrl.startsWith('/api')) {
+    req.originalUrl = '/api' + req.originalUrl;
   }
   next();
 });

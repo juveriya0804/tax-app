b90a21b5f1be7076c5b65be91533b9b4c8f5a9ba
Your Name <you@example.com>
2026-06-29 14:47:24 +0530

    Restore basePath in serverless-http

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,4 +1,17 @@
 import serverless from 'serverless-http';
+import express from 'express';
 import app from '../../src/app';
 
-export const handler = serverless(app);
+const netlifyApp = express();
+
+netlifyApp.all('*', (req, res, next) => {
+  if (req.url === '/test-netlify') {
+    res.json({ message: 'Netlify function is working and intercepting requests!', url: req.url, originalUrl: req.originalUrl });
+  } else {
+    next();
+  }
+});
+
+netlifyApp.use(app);
+
+export const handler = serverless(netlifyApp);
